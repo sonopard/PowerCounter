8607d1b556aebe30349693a4cc2234a5327f4500
Christian Fischer <sw-dev@computerlyrik.de>
2013-08-16 22:48:25 +0200

    added import bean and set hibernate to memory

---
+++ webapp/src/main/java/de/labor23/powercounter/ImportBean.java
@@ -0,0 +1,55 @@
+package de.labor23.powercounter;
+
+import java.util.Date;
+
+import javax.annotation.PostConstruct;
+
+import org.primefaces.model.chart.LineChartSeries;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import de.labor23.powercounter.dm.PowerMeter;
+import de.labor23.powercounter.dm.Tick;
+import de.labor23.powercounter.dm.User;
+import de.labor23.powercounter.dm.hardware.Bank;
+
+@Component
+@Transactional
+public class ImportBean {
+
+	@PostConstruct
+	public void setUp() throws Exception {
+		
+		User u = new User();
+		u.setUsername("user");
+		u.setPassword("user");
+		u.setEnabled(true);
+		u.persist();
+		
+		PowerMeter p = new PowerMeter();
+		p.setAddress((byte) 23);
+		p.setBank(Bank.LOWER_BANK);
+		p.setMeterName("AV labordomäne");
+		p.persist();
+		
+		u.getPowerMeters().add(p);
+		u.merge();	
+		
+
+		Tick t;
+        Date now = new Date();
+        Date tickdate;
+
+    	Integer timeDelta = 6;
+    	Integer datapoints = 3600;
+    	
+        for(int i = 0; i<datapoints; i++) {
+        	
+        	t = new Tick();
+        	tickdate = new Date(now.getTime()-(timeDelta*(datapoints-i)));
+        	t.setOccurence(tickdate);
+        	t.setMeter(p);
+        	t.persist();
+        }
+	}
+}
