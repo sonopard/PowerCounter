937eef6fd45e05c8496358f6078a5b09580cc924
Christian Fischer <sw-dev@computerlyrik.de>
2013-09-03 22:06:27 +0200

    fix, made interrupts according to current connections

--- raspi/src/test_mcp23017.py
+++ raspi/src/test_mcp23017.py
@@ -3,13 +3,13 @@ import MCP23017
 import unittest
 
 class ChipTest(unittest.TestCase):
-  chip1 = MCP23017.MCP23017(0x20, {'A': 0x14, 'B': 0x16})
-  chip2 = MCP23017.MCP23017(0x20, {'A': 0x15, 'B': 0x17})
+  chip1 = MCP23017.MCP23017(0x20, {'A': 17}#, 'B': 0x00})
+  chip2 = MCP23017.MCP23017(0x21, {'A': 27}#, 'B': 0x00})
   def test_enable_interrupts(self):
-    chip1.activate_interrupts()
-    chip2.activate_interrupts()
-    chip1.activate_mirror()
-    chip2.activate_mirror()
+    self.chip1.activate_interrupts()
+    self.chip2.activate_interrupts()
+    self.chip1.activate_mirror()
+    self.chip2.activate_mirror()
 
 if __name__ == '__main__':
     unittest.main()
