2b6747ea43cd5fb80ee0437be5db32ff0567b8f4
Christian Fischer <sw-dev@computerlyrik.de>
2013-09-04 19:49:19 +0200

    added logging to read bytes

--- raspi/src/read_chip.py
+++ raspi/src/read_chip.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 from MCP23017 import MCP23017
+import logging
 
-
+logging.getLogger( "MCP23017" ).setLevel( logging.DEBUG )
 chips = [MCP23017(0x20, {'A': 17}),
           MCP23017(0x21, {'A': 27})]
 
 for chip in chips:
   for i in range(0x1B):
     byte = chip.read(i)
-    print("0b{0:b}".format(byte))
