033557f9b91592deb62a24d9ba90364c1175b4d5
Christian Fischer <sw-dev@computerlyrik.de>
2013-09-04 19:22:29 +0200

    fixed syntax

--- raspi/src/read_chip.py
+++ raspi/src/read_chip.py
@@ -2,8 +2,8 @@
 from MCP23017 import MCP23017
 
 
-chips = [MCP23017(0x20, {'A': 17})#, 'B': 0x00}),
-          MCP23017(0x21, {'A': 27})#, 'B': 0x00})]
+chips = [MCP23017(0x20, {'A': 17}),
+          MCP23017(0x21, {'A': 27})]
 
 for chip in chips:
   for i in range(0x1B):
