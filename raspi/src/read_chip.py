c9296d71623495a72c9b5694f60073ebb3edfd8e
Christian Fischer <sw-dev@computerlyrik.de>
2013-09-04 19:21:19 +0200

    blubb

--- raspi/src/read_chip.py
+++ raspi/src/read_chip.py
@@ -1,9 +1,9 @@
-import MCP23017
+#!/usr/bin/python3
+from MCP23017 import MCP23017
 
 
-chips = {}
-chips[1] = MCP23017(0x20, {'A': 17})#, 'B': 0x00})
-chips[2] = MCP23017(0x21, {'A': 27})#, 'B': 0x00})
+chips = [MCP23017(0x20, {'A': 17})#, 'B': 0x00}),
+          MCP23017(0x21, {'A': 27})#, 'B': 0x00})]
 
 for chip in chips:
   for i in range(0x1B):
