1475f39e7ddfac4458aedbecfd22e268e49f4bef
Christian Fischer <sw-dev@computerlyrik.de>
2013-09-04 20:34:23 +0200

    added logger for main

--- raspi/src/main.py
+++ raspi/src/main.py
@@ -34,6 +34,8 @@ ticks_queue = Queue()
 logging.basicConfig()
 logging.getLogger( "MCP23017" ).setLevel( logging.DEBUG )
 logging.getLogger( "PC4004B" ).setLevel( logging.DEBUG )
+log = logging.getLogger("PowerCounter")
+log.setLevel(logging.DEBUG)
 
 #SET UP SHIELD
 chip1 = MCP23017(0x20, {'A': 17})#, 'B': 0x00})
